ecfe12704e55eded93caf1f045537c93fcb350e2
Oscar Gonzalez <oscarlidenbrock@gmail.com>
2024-06-17 22:07:39 +0000

    add friendly paths

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -3,3 +3,6 @@
 require '../vendor/autoload.php';
 require '../lib/definitions.php';
 require '../lib/functions.php';
+
+$core = new NextFramework\Core\Core();
+$core->init();
--- public/index.php
+++ public/index.php
@@ -1,11 +1,8 @@
 <?php
 
-use Symfony\Component\Yaml\Yaml;
-
 require('../lib/bootstrap.php');
 
-$core = new NextFramework\Core\Core();
-$core->init();
+
 
 
 $core->logger->log('¡Hola mundo! (solo se muestra en modo debug (TYPE_DEBUG)', TYPE_INFO, OUTPUT_ALL);
